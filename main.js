fbb4764b572cc6f0252689dbd33c90c9d4b0bf03
Margarita Khodzka <Margarita.Khodzka@meredith.com>
2024-06-27 12:12:17 -0400

    L-10 communicating events end code and challenge solution

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        reduceCart(id) {
+            const index = this.cart.indexOf(id)
+            console.log(index)
+            if (index > -1) {
+                this.cart.splice(index, 1);
+            }
+        }
+    }
 })
